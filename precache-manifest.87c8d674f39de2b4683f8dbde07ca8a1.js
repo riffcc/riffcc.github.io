16b16f9f28ca4f9f0916b4cde9768f6e83364110
Benjamin Arntzen <zorlin@gmail.com>
2021-02-15 10:29:38 +0800

    Add website

---
+++ precache-manifest.87c8d674f39de2b4683f8dbde07ca8a1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a2c30315bedcae74bba11df22922ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20a688074be19953c421",
+    "url": "/static/css/main.9b3ac610.chunk.css"
+  },
+  {
+    "revision": "da5e300595262343f678",
+    "url": "/static/js/2.370c3799.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.370c3799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a688074be19953c421",
+    "url": "/static/js/main.170798f7.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "f0d56404ecd79d49d1970fe83c04cce1",
+    "url": "/static/media/bolt-solid.f0d56404.svg"
+  },
+  {
+    "revision": "f3e11abadbc21a1d06e3e92d21ca25a1",
+    "url": "/static/media/chart-bar-solid.f3e11aba.svg"
+  },
+  {
+    "revision": "0eebeda882035630a909236fe39382c9",
+    "url": "/static/media/creative-commons-brands.0eebeda8.svg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "79b0c06d5028af1b24b52c322ffa18fc",
+    "url": "/static/media/fist-raised-solid.79b0c06d.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
